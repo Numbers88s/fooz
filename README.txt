a7b3fac9ddd75199709c0fb7ff33fffcefee1724
Cesar Jimenez <cesar.jimenez@metaltoad.com>
2014-10-24 09:38:11 -0700

    New Code

---
+++ README.txt
@@ -0,0 +1,16 @@
+Keyboard input
+
+Number 1 = right score
+
+Letter B = left score
+
+Letter O = change left avatar -->
+Letter P = change left avatar  <---
+
+Letter Q = change right avatar -->
+Letter R = change right avatar  <---
+
+Letter C = change between "Toad Tournament" and "Foosball"
+
+Letter W = resets game
+
--- app/script.js
+++ app/script.js
@@ -16,18 +16,61 @@ function ctrl($scope, $timeout, $interval){
   $scope.message2 = "";
   $scope.finishthem = false;
   $scope.timerprom;
+  $scope.Timer;
+  $scope.TotalSeconds;
 
-  $scope.startTimer = function () {
-    $scope.timerprom = $interval(function(){
-      if($scope.second < 59) {
-       $scope.second++;
-      }
-      else {
-        $scope.second = 0;
-        $scope.minute++;
-      }
-    }, 1000);
-  }
+  // $scope.startTimer = function () {
+  //   $scope.timerprom = $interval(function(){
+  //     if ($scope.second < 59) {
+  //       $scope.second++;
+  //     }
+  //     else {
+  //       $scope.second = 0;
+  //       $scope.minute++;
+  //     }
+  //   }, 1000);
+  // 
+
+  // $scope.CreateTimer = function(TimerID, Time) {
+  //   $scope.Timer = document.getElementById(TimerID);
+  //   $scope.TotalSeconds = Time;
+
+  //   $scope.UpdateTimer()
+  //   window.setTimeout("Tick()", 1000);
+  // }
+
+  // $scope.Tick = function() {
+  //   if ($scope.TotalSeconds <= 0) {
+  //     alert("Time's up!")
+  //     return;
+  //   }
+
+  //   TotalSeconds += 1;
+  //   $scope.UpdateTimer()
+  //   window.setTimeout("Tick()", 1000);
+  // }
+
+  // $scope.UpdateTimer = function() {
+  //   var Seconds = TotalSeconds;
+    
+  //   var Hours = Math.floor(Seconds / 3600);
+  //   Seconds -= Hours * (3600);
+
+  //   var Minutes = Math.floor(Seconds / 60);
+  //   Seconds -= Minutes * (60);
+
+  //   var TimeStr = LeadingZero(Minutes) + " : " + LeadingZero(Seconds)
+
+  //   Timer.innerHTML = TimeStr;
+  // }
+
+  // $scope.LeadingZero = function(Time) {
+  //   return (Time < 10) ? "0" + Time : + Time;
+  // }
+
+  // $scope.RestartTimer = function() {
+  //   $scope.TotalSeconds = 0;
+  // }
 
   $scope.recordInput = function($e) {
     
@@ -36,13 +79,17 @@ function ctrl($scope, $timeout, $interval){
   	}
 
   	//rotate player 1
-	  if($e.which == 111) {
-	   $scope.avatar1++;
+	  if($e.which == 112) {
+      $scope.avatar2++;
+    } else if($e.which == 111 && $scope.avatar2 > 0) {
+      $scope.avatar2--;
     }
 
     //rotate player 2
-	  if($e.which == 112) {
-	    $scope.avatar2++;
+	  if($e.which == 114) {
+	    $scope.avatar1++;
+    } else if($e.which == 113 && $scope.avatar1 > 0) {
+      $scope.avatar1--;
     }
 
     //score player 1
@@ -56,7 +103,7 @@ function ctrl($scope, $timeout, $interval){
 
         if($scope.player1 == 4 && $scope.player2 < 4) {
           $scope.finishthem = true;
-          $timeout(function(){$scope.finishthem = false;}, 2000);
+          $timeout(function(){$scope.finishthem = false;}, 3500);
         }
       }
       //$scope.scoreInterval = true;
@@ -71,7 +118,7 @@ function ctrl($scope, $timeout, $interval){
     //score player 2
     if($e.which == 49) {
       if(!$scope.scoreInterval) {
-        $timeout(function(){$scope.scoreInterval = false}, 4000);
+        $timeout(function(){$scope.scoreInterval = false}, 3500);
         $timeout(function(){$scope.goal = false}, 2000);
         $scope.player2++;
         $scope.goal = true;
@@ -80,13 +127,13 @@ function ctrl($scope, $timeout, $interval){
 
         if($scope.player2 == 4 && $scope.player1 < 4) {
           $scope.finishthem = true;
-          $timeout(function(){$scope.finishthem = false;}, 2000);
+          $timeout(function(){$scope.finishthem = false;}, 4000);
         }
       }
       //$scope.scoreInterval = true;
     }
 
-    //reset key
+    //reset key (Press W)
     if($e.which == 119) {
       $scope.reset();
     }
@@ -97,8 +144,8 @@ function ctrl($scope, $timeout, $interval){
 	 
   }
 
-    if($scope.player1 == 5) {
-    	//WINNER
+    if($scope.player2 == 5) {
+    //WINNER
     	$scope.message1 = "WINNER!";
     	if($scope.player2 == 0) {
     		//SKUNK!
@@ -118,8 +165,8 @@ function ctrl($scope, $timeout, $interval){
         }
       }
     }
-    else if($scope.player2 == 5) {
-    	//winner!
+    else if($scope.player1 == 5) {
+    //WINNER!
 		  $scope.message2 = "WINNER!";
 		  if($scope.player1 == 0) {
     		//SKUNK!
@@ -155,8 +202,124 @@ function ctrl($scope, $timeout, $interval){
   }
 
   $scope.reset = function() {
-  	$scope.nextRound();
+  	// $scope.nextRound();
     $scope.player1Rounds = 0;
     $scope.player2Rounds = 0;
+    $scope.avatar1 = 0;
+    $scope.avatar2 = 0;
+    $scope.player1 = 0;
+    $scope.player2 = 0;
+    // $scope.startTimer();
   }
 }
+
+var Timer;
+var TotalSeconds;
+var stop;
+var restart;
+
+function CreateTimer(TimerID, Time) {
+  Timer = document.getElementById(TimerID);
+  TotalSeconds = Time;
+
+  UpdateTimer()
+  restart = window.setTimeout("Tick()", 1000);
+}
+
+function Tick() {
+  if (TotalSeconds === 1500) {
+    return;
+  }
+
+  TotalSeconds += 1;
+  UpdateTimer()
+  stop = window.setTimeout("Tick()", 1000);
+  return;
+}
+
+function UpdateTimer() {
+  var Seconds = TotalSeconds;
+  
+  var Hours = Math.floor(Seconds / 3600);
+  Seconds -= Hours * (3600);
+
+  var Minutes = Math.floor(Seconds / 60);
+  Seconds -= Minutes * (60);
+
+  var TimeStr = LeadingZero(Minutes) + " : " + LeadingZero(Seconds);
+
+  Timer.innerHTML = TimeStr;
+}
+
+function LeadingZero(Time) {
+  return (Time < 10) ? "0" + Time : + Time;
+}
+
+document.onkeypress = function(e) {
+  // If user presses W it resets the score and picture  
+  if(e.keyCode == 119) {
+    clearTimeout(stop);
+    clearTimeout(restart);
+    Timer.innerHTML = "00 : 00";
+    CreateTimer("timer", 01);
+    e.preventDefault();
+  //If user presses s it reloads page
+  } else if(e.keyCode == 115) {
+    document.location.reload(true);
+  } else if(e.keyCode == 102) {
+    clearTimeout(stop);
+    clearTimeout(restart);
+  }
+}
+
+// IGNORE THIS FOR NOW
+// window.addEventListener("keydown", keysPressed, false);
+// // window.addEventListener("keyup", keysReleased, false);
+
+// var keys = [];
+
+// function keysPressed(e) {
+//   // This stores an entry for every key pressed.
+//   keys[e.keyCode] = true;
+
+//   // w + w
+//   if (keys[115] && keys[115]) {
+//     console.log("it works!");
+//     keys[e.keyCode] = false;
+//   }
+// }
+
+// function  keysReleased(e) {
+//   // This marks keys that were released.
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
